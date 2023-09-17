4201762c7c2004cefeb83badc991fb143f4305b8
omaomach <aaronjoash44@gmail.com>
2023-09-17 12:48:45 +0300

    completed ViewPagerApp

--- app/src/main/java/com/omao/viewpagerapp/MainActivity.kt
+++ app/src/main/java/com/omao/viewpagerapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.omao.viewpagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.omao.viewpagerapp.Adapter.MyAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var myAdapter: MyAdapter
+    lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.view_pager)
+
+        myAdapter = MyAdapter(this)
+
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        viewPager.adapter = myAdapter
+
     }
 }
